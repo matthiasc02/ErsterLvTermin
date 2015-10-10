026ae41aa13d85944067e581b8f2d35739b1ce7e
matthiasc02 <king.matl.ms@gmail.com>
2015-10-10 15:10:42 +0200

    Variablen

--- src/Demo.java
+++ src/Demo.java
@@ -3,14 +3,16 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		boolean treeFront=true;
-		int numberLeaves=0;
-		
-		System.out.println("Ist ein Baum voraus? "+treeFront);
-		System.out.println("Anzahl der Blätter: "+numberLeaves);
-		
-		
+
+		boolean treeFront = true;
+		int numberLeaves = 0;
+		char meinZeichen = 'X';
+
+		//System.out.println("Ist ein Baum voraus? " + treeFront);
+		System.out.println("Ist ein Baum voraus? " + treeFront+"\n"+"Anzahl der Blätter: " + numberLeaves);
+		//System.out.println("Anzahl der Blätter: " + numberLeaves);
+		System.out.println(meinZeichen);
+
 
 	}
 
