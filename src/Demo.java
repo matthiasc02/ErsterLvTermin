a1bb1d69bacb2299dca6e00f4be4b35f9f07703f
matthiasc02 <king.matl.ms@gmail.com>
2015-10-10 14:43:17 +0200

    neu

---
+++ src/Demo.java
@@ -0,0 +1,17 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean treeFront=true;
+		int numberLeaves=0;
+		
+		System.out.println("Ist ein Baum voraus? "+treeFront);
+		System.out.println("Anzahl der Blätter: "+numberLeaves);
+		
+		
+
+	}
+
+}
