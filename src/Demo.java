73ba697bb9c6acce32a63fcbd60af6896d39250e
matthiasc02 <king.matl.ms@gmail.com>
2015-10-10 15:18:17 +0200

    +Rechnen

--- src/Demo.java
+++ src/Demo.java
@@ -7,11 +7,22 @@ public class Demo {
 		boolean treeFront = true;
 		int numberLeaves = 0;
 		char meinZeichen = 'X';
+		int a=9;
+		int b=5;
+		
+		numberLeaves=numberLeaves+1;
 
 		//System.out.println("Ist ein Baum voraus? " + treeFront);
 		System.out.println("Ist ein Baum voraus? " + treeFront+"\n"+"Anzahl der Blätter: " + numberLeaves);
 		//System.out.println("Anzahl der Blätter: " + numberLeaves);
 		System.out.println(meinZeichen);
+		
+		System.out.println(a+"+"+b+"="+(a+b));
+		System.out.println(a+"-"+b+"="+(a-b));
+		System.out.println(a+"*"+b+"="+(a*b));
+		System.out.println(a+"/"+b+"="+(a/b));
+		System.out.println(a+"%"+b+"="+(a%b));
+		
 
 
 	}
